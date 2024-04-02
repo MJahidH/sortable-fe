7c01a86a1cb939ad4a78083d6224eb383c5fc2bb
MJahidH <m.jahid.h@hotmail.com>
2024-04-02 23:25:18 +0100

    button changes to green when clicked for each to do item

--- SortAble/components/ToDoPage.jsx
+++ SortAble/components/ToDoPage.jsx
@@ -1,19 +1,57 @@
-import React from "react";
-import { View, Text, Pressable } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, Text, Pressable, SwitchBase } from "react-native";
 
-export default function ToDoPage({ style, fileContent,buttonStyle }) {
+export default function ToDoPage({ style, fileContent }) {
+  const [doneStatus, setDoneStatus] = useState(
+    Array(fileContent.length).fill(false)
+  );
+  
+
+  const toggleIsDone = (index) => {
+    const newDoneStatus = [...doneStatus];
+    newDoneStatus[index] = !newDoneStatus[index];
+    setDoneStatus(newDoneStatus);
+  };
+
   return (
     <View>
-      {fileContent.map((toDoItem) => {
+      {fileContent.map((toDoItem, index) => {
         return (
-          <View>
-            <Pressable style={buttonStyle}>
-
-            </Pressable>
+          <View 
+          key={index}>
+            <Pressable
+              style={
+                doneStatus[index] ? buttonStyles.isDone : buttonStyles.notDone
+              }
+              onPress={() => {
+                toggleIsDone(index);
+              }}
+            ></Pressable>
             <Text style={style}>{toDoItem.title}</Text>
+
           </View>
         );
       })}
     </View>
   );
 }
+const buttonStyles = StyleSheet.create({
+  notDone: {
+    height: 38,
+    width: 38,
+    padding: 10,
+    borderRadius: 50,
+    backgroundColor: "transparent",
+    borderColor: `#FFF`,
+    borderWidth: 4,
+  },
+  isDone: {
+    height: 38,
+    width: 38,
+    padding: 10,
+    borderRadius: 50,
+    backgroundColor: "",
+    backgroundColor: "#00FF00",
+    borderWidth: 4,
+  },
+});
