7ec63267b2de3f59de7060f51f7b7ec32423945d
MJahidH <m.jahid.h@hotmail.com>
2024-04-04 17:20:36 +0100

    text input gets aded to the newdoneitem state object on the key title

--- SortAble/components/AddNewToDoItem.jsx
+++ SortAble/components/AddNewToDoItem.jsx
@@ -1,12 +1,41 @@
-import React, { useState } from "react";
-import { View,Text, TextInput } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 
-export default function AddNewToDoItem ({style}) {
-    return (
-        <View>
-            <Text style={style}>this is the add new item </Text>
-            <TextInput
-            placeholder="add new item"/>
-        </View>
-    )
+export default function AddNewToDoItem({ style }) {
+  const [newDoneItem, setNewDoneItem] = useState({
+    title: "",
+    isDone: false,
+  });
+  useEffect(() => {
+    console.log(newDoneItem);
+  }, [newDoneItem]);
+
+  
+  const handleChange = (event) => {
+    setNewDoneItem({
+        ...newDoneItem,
+        title : event
+    })
+  };
+
+  return (
+    <View>
+      <Text style={style}>this is the add new item </Text>
+      <TextInput
+        style={[style, styles.textInputcontainer]}
+        keyboardAppearance="dark"
+        placeholder="add new item"
+        placeholderTextColor="#FFFF00"
+        onChangeText={handleChange}
+      />
+    </View>
+  );
 }
+const styles = StyleSheet.create({
+  textInputcontainer: {
+    borderColor: "#FFFF00",
+    borderWidth: 1,
+    padding: 10,
+    margin: 10,
+  },
+});
